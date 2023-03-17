1b61ee6f4cddd77048abb4603156952b794835cc
sam <shinhs91@gmail.com>
2023-03-17 17:02:35 +0900

    [Feat] SpringSecurityConfig 초기 구현

---
+++ src/main/java/com/numble/mybox/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.numble.mybox.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .antMatchers("").permitAll().anyRequest().authenticated()
+//                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+}
